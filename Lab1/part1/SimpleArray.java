52492d4f223c8960a2252aa8f49152621b62d4aa
Tristan <tcavarno@gmail.com>
2020-04-14 15:51:51 -0700

    bug fixes

--- Lab1/part1/SimpleArray.java
+++ Lab1/part1/SimpleArray.java
@@ -5,9 +5,14 @@ class SimpleArray
       
       int [] newValues = new int[values.length];  // This allocates an array of integers.
 
-      for(int i = 0; i<values.length; i++){
-         newValues[i] = values[(int)Math.pow(i,2)];
-      }
+      for(int i = 0; i<values.length; i++) newValues[i] = (int)Math.pow(values[i],2);
+
       return newValues;
    }
+   /*
+   public static void main(String args[]){
+      int[] l = new int[] {1,2,3,4,5,6};
+      System.out.print(SimpleArray.squareAll(l) == new int[] {1,4,8,16,25,36});
+   }
+   */
 }
