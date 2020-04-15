c5a0bf110739e886b0124c6f8098ea33ef6d5301
Tristan <tcavarno@gmail.com>
2020-04-15 09:01:55 -0700

    a

--- Lab1/part2/TestCases.java
+++ Lab1/part2/TestCases.java
@@ -56,7 +56,7 @@ public class TestCases
       assertEquals(3*Math.PI/2, new Point(0, -1).getAngle(), DELTA);
       assertEquals(Math.PI, new Point(-3, 0).getAngle(), DELTA);
       assertEquals(3.92699081, new Point(-2.0, -2.0).getAngle(), DELTA);
-      assertEquals(5.497787144, new Point(-2.0, 2.0).getAngle(), DELTA);
+      assertEquals(5.497787144, new Point(2.0, -2.0).getAngle(), DELTA);
       
    
    }
